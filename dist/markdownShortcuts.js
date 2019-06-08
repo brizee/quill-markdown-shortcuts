3df96aa2c5d8cee82b037ab1d62f1546f6b6db16
Brian Beacom <brian@brianbea.com>
2019-06-08 19:27:16 +0100

    Attach fix

--- dist/markdownShortcuts.js
+++ dist/markdownShortcuts.js
@@ -109,6 +109,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+if (!Quill) {
+  var Quill = window.Quill;
+}
 Quill.register('formats/horizontal', _hr2.default);
 
 var MarkdownShortcuts = function () {
